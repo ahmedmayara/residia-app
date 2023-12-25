bce9e9081ac385e1f2990418dbed17211407c405
Ahmed <ahmedmayara789@gmail.com>
2023-12-25 01:30:49 +0100

    feat: update listing-card component with carousel and favorite button

--- components/listing-card.tsx
+++ components/listing-card.tsx
@@ -2,27 +2,23 @@
 
 import React from "react";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
-
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
 import { useRouter } from "next/navigation";
 import { Booking, Listing, User } from "@prisma/client";
 import Image from "next/image";
 import { FavoriteButton } from "./favorite-button";
 import { useCountries } from "@/hooks/useCountries";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
 
 interface ListingCardProps {
   listing: Listing;
   host?: User;
   booking?: Booking;
-  onAction?: (id: string) => void;
-  disabled?: boolean;
-  actionLabel?: string;
-  actionId?: string;
   currentUser?: User | null;
 }
 
@@ -30,10 +26,6 @@ export function ListingCard({
   listing,
   host,
   booking,
-  onAction,
-  disabled,
-  actionLabel,
-  actionId,
   currentUser,
 }: ListingCardProps) {
   const router = useRouter();
@@ -64,40 +56,29 @@ export function ListingCard({
   }, [listing, getByValue]);
 
   return (
-    <div
-      onClick={() => {
-        if (disabled) return;
-        router.push(`/listings/${listing.id}`);
-      }}
-      className="group col-span-1 cursor-pointer"
-    >
+    <div className="group col-span-1 cursor-pointer">
       <div className="flex w-full flex-col">
-        <Swiper
-          modules={[Autoplay]}
-          autoplay={{
-            delay: 5000,
-          }}
-          className="relative aspect-square w-full overflow-hidden rounded-xl bg-gray-200"
-        >
-          {listing.images.map((image) => (
-            <SwiperSlide key={image}>
-              <Image
-                src={image}
-                alt="Listing image"
-                width={500}
-                height={500}
-                className="h-full w-full rounded-xl object-cover object-center"
-              />
-              <div className="absolute right-3 top-3">
-                <FavoriteButton
-                  listingId={listing.id}
-                  currentUser={currentUser}
+        <Carousel className="relative aspect-square w-full overflow-hidden rounded-xl">
+          <CarouselContent>
+            {listing.images.map((photo) => (
+              <CarouselItem key={photo} className="relative h-[300px] w-full">
+                <Image
+                  src={photo}
+                  alt={photo}
+                  fill
+                  sizes="100vw"
+                  className="h-auto w-full object-cover object-center"
+                  onClick={() => router.push(`/listings/${listing.id}`)}
                 />
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="absolute left-3 top-1/2 z-10 hidden -translate-y-1/2 transform opacity-75 transition-transform duration-200 ease-in-out hover:scale-110 hover:opacity-100 group-hover:flex" />
+          <CarouselNext className="absolute right-3 top-1/2 z-10 hidden -translate-y-1/2 transform opacity-75 transition-transform duration-200 ease-in-out hover:scale-110 hover:opacity-100 group-hover:flex" />
+          <div className="absolute right-3 top-3">
+            <FavoriteButton listingId={listing.id} currentUser={currentUser} />
+          </div>
+        </Carousel>
         <div className="mt-2 flex flex-col text-sm">
           <h1 className="font-medium">
             {location?.region}, {location?.label}
